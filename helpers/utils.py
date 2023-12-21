5a2360415bf827f32a9c01736be0109f04bd36a6
notjawad <bawskiya@gmail.com>
2023-12-21 16:33:40 -0600

    Add utility function to fetch latest commit info

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,5 +1,14 @@
 import discord
 import aiosqlite
+import aiohttp
+
+from datetime import datetime, timezone
+
+
+def iso_to_discord_timestamp(iso_date):
+    date_obj = datetime.fromisoformat(iso_date.rstrip("Z")).replace(tzinfo=timezone.utc)
+    timestamp = int(date_obj.timestamp())
+    return f"<t:{timestamp}:R>"
 
 
 async def log(guild: discord.Guild, embed: discord.Embed, conn: aiosqlite.Connection):
@@ -15,3 +24,19 @@ async def log(guild: discord.Guild, embed: discord.Embed, conn: aiosqlite.Connec
         await channel.send(embed=embed)
     else:
         return
+
+
+async def fetch_latest_commit_info():
+    repo_url = "https://api.github.com/repos/notjawad/uni/commits/main"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(repo_url) as response:
+            if response.status != 200:
+                return "Error: Unable to fetch commit info"
+            json_response = await response.json()
+            return {
+                "id": json_response["sha"],
+                "message": json_response["commit"]["message"],
+                "author": json_response["commit"]["committer"]["name"],
+                "date": json_response["commit"]["committer"]["date"],
+                "url": json_response["html_url"],
+            }
