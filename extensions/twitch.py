a365da5e0165b436276d16254cd8d06768d77506
notjawad <bawskiya@gmail.com>
2023-12-22 13:19:47 -0600

    Add Twitch stream checking and notification functionality

--- extensions/twitch.py
+++ extensions/twitch.py
@@ -1,8 +1,9 @@
 import discord
 import aiosqlite
 import yaml
+import aiohttp
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 
 def load_config():
@@ -19,6 +20,7 @@ class Twitch(commands.Cog):
         self.bot = bot_
         self.db_path = "kino.db"
         self.bot.loop.create_task(self.setup_db())
+        self.check_streams.start()
 
     async def setup_db(self):
         self.conn = await aiosqlite.connect(self.db_path)
@@ -51,6 +53,18 @@ class Twitch(commands.Cog):
                     f"Command `{command_name}` is disabled."
                 )
 
+    async def get_stream(self, twitch_channel: str):
+        headers = {
+            "Client-ID": config["TWITCH"]["CLIENT_ID"],
+            "Authorization": f"Bearer {config['TWITCH']['ACCESS_TOKEN']}",
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"https://api.twitch.tv/helix/streams?user_login={twitch_channel}",
+                headers=headers,
+            ) as response:
+                return await response.json() if response.status == 200 else None
+
     _twitch = discord.commands.SlashCommandGroup(
         name="twitch", description="Twitch related commands."
     )
@@ -59,42 +73,150 @@ class Twitch(commands.Cog):
         name="enable",
         description="Enables Twitch notifications",
     )
+    @commands.has_permissions(administrator=True)
     async def _enable(
         self,
         ctx: discord.ApplicationContext,
         channel: discord.Option(
-            name="channel",
-            description="The channel to send notifications to.",
-            required=True,
-            autocomplete=channel_autocomplete,
+            discord.TextChannel, "Select a channel", autocomplete=channel_autocomplete
         ),
     ):
-        ...
+        async with self.conn.cursor() as cur:
+            await cur.execute(
+                "INSERT INTO twitch_notifications (guild_id, channel_id) VALUES (?, ?)",
+                (ctx.guild.id, channel.id),
+            )
+            await self.conn.commit()
+
+        embed = discord.Embed(
+            description=f"Twitch notifications will now be sent to {channel.mention}.",
+            color=config["COLORS"]["BLURPLE"],
+        )
+        await ctx.respond(embed=embed)
 
     @_twitch.command(
         name="disable",
         description="Disables Twitch notifications",
     )
+    @commands.has_permissions(administrator=True)
     async def _disable(
         self,
         ctx: discord.ApplicationContext,
     ):
-        ...
+        async with self.conn.cursor() as cur:
+            await cur.execute(
+                "DELETE FROM twitch_notifications WHERE guild_id = ?", (ctx.guild.id,)
+            )
+            await self.conn.commit()
+
+        embed = discord.Embed(
+            description="Twitch notifications have been disabled.",
+            color=config["COLORS"]["SUCCESS"],
+        )
+        await ctx.respond(embed=embed)
 
     @_twitch.command(
         name="add",
         description="Adds a Twitch channel to the notification list.",
     )
+    @commands.has_permissions(administrator=True)
     async def _add(
         self,
         ctx: discord.ApplicationContext,
-        channel: discord.Option(
+        channel_name: discord.Option(
             name="channel",
             description="The channel to add to the notification list.",
             required=True,
         ),
     ):
-        ...
+        async with self.conn.cursor() as cur:
+            await cur.execute(
+                "INSERT INTO twitch_streamers (channel_name, guild_id) VALUES (?, ?)",
+                (channel_name, ctx.guild.id),
+            )
+            await self.conn.commit()
+
+        embed = discord.Embed(
+            description=f"Added `{channel_name}` to the notification list.",
+            color=config["COLORS"]["BLURPLE"],
+        )
+        await ctx.respond(embed=embed)
+
+    @tasks.loop(seconds=10)
+    async def check_streams(self):
+        async with self.conn.cursor() as cur:
+            await cur.execute(
+                """
+                SELECT ts.channel_name, ts.guild_id, tn.channel_id, ts.is_live, ts.notification_sent
+                FROM twitch_streamers ts
+                INNER JOIN twitch_notifications tn ON ts.guild_id = tn.guild_id
+            """
+            )
+            streamers = await cur.fetchall()
+
+            for streamer in streamers:
+                (
+                    channel_name,
+                    guild_id,
+                    notification_channel_id,
+                    is_live,
+                    notification_sent,
+                ) = streamer
+                stream_data = await self.get_stream(channel_name)
+
+                currently_live = (
+                    len(stream_data.get("data", [])) > 0 if stream_data else False
+                )
+
+                if currently_live and not notification_sent:
+                    if guild := self.bot.get_guild(int(guild_id)):
+                        if channel := guild.get_channel(int(notification_channel_id)):
+                            embed = discord.Embed(
+                                description=f"**{stream_data['data'][0]['user_name']}** is now live on Twitch!",
+                                color=config["COLORS"]["BLURPLE"],
+                            )
+                            embed.set_image(
+                                url=stream_data["data"][0]["thumbnail_url"].format(
+                                    width=1280, height=720
+                                )
+                            )
+                            embed.add_field(
+                                name="Stream Title",
+                                value=stream_data["data"][0]["title"],
+                            )
+                            embed.add_field(
+                                name="Game",
+                                value=stream_data["data"][0]["game_name"],
+                            )
+                            embed.add_field(
+                                name="Viewers",
+                                value=f"{stream_data['data'][0]['viewer_count']:,}",
+                            )
+                            watch_button = discord.ui.Button(
+                                style=discord.ButtonStyle.link,
+                                label="Watch",
+                                url=f"https://twitch.tv/{channel_name}",
+                            )
+                            view = discord.ui.View()
+                            view.add_item(watch_button)
+
+                            await channel.send(embed=embed, view=view)
+                            await cur.execute(
+                                "UPDATE twitch_streamers SET notification_sent = TRUE WHERE channel_name = ?",
+                                (channel_name,),
+                            )
+                            await cur.execute(
+                                "UPDATE twitch_streamers SET is_live = TRUE WHERE channel_name = ?",
+                                (channel_name,),
+                            )
+
+                elif not currently_live and is_live:
+                    await cur.execute(
+                        "UPDATE twitch_streamers SET is_live = FALSE, notification_sent = FALSE WHERE channel_name = ?",
+                        (channel_name,),
+                    )
+
+            await self.conn.commit()
 
 
 def setup(bot_: discord.Bot):
