43e82b42ab58927c6d7fb2c27431d2560166ecca
notjawad <bawskiya@gmail.com>
2023-12-22 17:00:53 -0600

    Add BumpReminder cog

---
+++ extensions/bump_reminder.py
@@ -0,0 +1,50 @@
+import discord
+import aiosqlite
+import yaml
+
+from discord.ext import commands
+
+
+def load_config():
+    with open("config.yml", "r", encoding="utf-8") as config_file:
+        config = yaml.safe_load(config_file)
+    return config
+
+
+config = load_config()
+
+
+class BumpReminder(commands.Cog):
+    def __init__(self, bot_: discord.Bot):
+        self.bot = bot_
+        self.db_path = "kino.db"
+        self.bot.loop.create_task(self.setup_db())
+
+    async def setup_db(self):
+        self.conn = await aiosqlite.connect(self.db_path)
+
+    async def cog_before_invoke(self, ctx: discord.ApplicationContext):
+        command_name = ctx.command.name
+
+        async with self.conn.cursor() as cur:
+            await cur.execute(
+                "SELECT 1 FROM disabled_commands WHERE command = ?", (command_name,)
+            )
+            if await cur.fetchone():
+                embed = discord.Embed(
+                    title="Command Disabled",
+                    description=f"The command `{command_name}` is currently disabled in this guild.",
+                    color=discord.Color.red(),
+                )
+                embed.set_footer(text="This message will be deleted in 10 seconds.")
+                embed.set_author(
+                    name=self.bot.user.display_name, icon_url=self.bot.user.avatar.url
+                )
+                await ctx.respond(embed=embed, ephemeral=True, delete_after=10)
+                raise commands.CommandInvokeError(
+                    f"Command `{command_name}` is disabled."
+                )
+
+
+def setup(bot_: discord.Bot):
+    bot_.add_cog(BumpReminder(bot_))
