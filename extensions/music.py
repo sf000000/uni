a6e1f97d247d25d08e01c84468e0e9451203b8c5
notjawad <bawskiya@gmail.com>
2023-12-22 20:17:53 -0600

    Add music extension

---
+++ extensions/music.py
@@ -0,0 +1,66 @@
+import discord
+import aiosqlite
+import yaml
+import wavelink
+
+from discord.ext import commands
+
+
+def load_config():
+    with open("config.yml", "r", encoding="utf-8") as config_file:
+        config = yaml.safe_load(config_file)
+    return config
+
+
+config = load_config()
+
+
+class CogName(commands.Cog):
+    def __init__(self, bot_: discord.Bot):
+        self.bot = bot_
+        self.db_path = "kino.db"
+        self.bot.loop.create_task(self.setup_db())
+        self.bot.loop.create_task(self.connect_nodes())
+
+    async def connect_nodes(self):
+        await self.bot.wait_until_ready()
+        nodes = [
+            wavelink.Node(
+                uri="http://localhost:2333",
+                password="youshallnotpass",
+            )
+        ]
+        await wavelink.Pool.connect(nodes=nodes, client=self.bot, cache_capacity=100)
+
+    @commands.Cog.listener()
+    async def on_wavelink_node_ready(self, payload: wavelink.NodeReadyEventPayload):
+        print(f"🎧 Wavelink node {payload.node.identifier} is ready.")
+
+    async def setup_db(self):
+        self.conn = await aiosqlite.connect(self.db_path)
+
+    async def cog_before_invoke(self, ctx: discord.ApplicationContext):
+        command_name = ctx.command.name
+
+        async with self.conn.cursor() as cur:
+            await cur.execute(
+                "SELECT 1 FROM disabled_commands WHERE command = ?", (command_name,)
+            )
+            if await cur.fetchone():
+                embed = discord.Embed(
+                    title="Command Disabled",
+                    description=f"The command `{command_name}` is currently disabled in this guild.",
+                    color=discord.Color.red(),
+                )
+                embed.set_footer(text="This message will be deleted in 10 seconds.")
+                embed.set_author(
+                    name=self.bot.user.display_name, icon_url=self.bot.user.avatar.url
+                )
+                await ctx.respond(embed=embed, ephemeral=True, delete_after=10)
+                raise commands.CommandInvokeError(
+                    f"Command `{command_name}` is disabled."
+                )
+
+
+def setup(bot_: discord.Bot):
+    bot_.add_cog(CogName(bot_))
