6ad8c1a135f3ffbacbdb6383c96391943283377a
Jawad <bawskiya@gmail.com>
2024-01-18 12:56:01 -0600

    feat(utils.py): add seaborn style and bar chart creation functions

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,6 +1,7 @@
 import discord
 import aiosqlite
 import aiohttp
+import seaborn as sns
 
 
 from datetime import datetime, timezone
@@ -53,3 +54,52 @@ async def is_premium(member: discord.Member, conn: aiosqlite.Connection) -> bool
 def create_progress_bar(value, max_blocks=10, full_block="█", empty_block="░"):
     filled_blocks = int(value * max_blocks)
     return full_block * filled_blocks + empty_block * (max_blocks - filled_blocks)
+
+
+def set_seaborn_style(font_family, background_color, grid_color, text_color):
+    sns.set_style(
+        {
+            "axes.facecolor": background_color,
+            "figure.facecolor": background_color,
+            "grid.color": grid_color,
+            "axes.edgecolor": grid_color,
+            "axes.grid": True,
+            "axes.axisbelow": True,
+            "axes.labelcolor": text_color,
+            "text.color": text_color,
+            "font.family": font_family,
+            "xtick.color": text_color,
+            "ytick.color": text_color,
+            "xtick.bottom": False,
+            "xtick.top": False,
+            "ytick.left": False,
+            "ytick.right": False,
+            "axes.spines.left": False,
+            "axes.spines.bottom": True,
+            "axes.spines.right": False,
+            "axes.spines.top": False,
+        }
+    )
+
+
+def create_bar_chart(row, ax):
+    num_colors = len(row)
+    random_colors = sns.color_palette("husl", n_colors=num_colors)
+
+    chart = sns.barplot(
+        y=row.index.str.capitalize().values,
+        x=row.values,
+        orient="h",
+        palette=random_colors,
+        hue=row.index.str.capitalize().values,
+        ax=ax,
+        saturation=1,
+        dodge=False,
+        width=0.75,
+        legend=False,
+    )
+
+    for index, value in enumerate(row.values):
+        ax.text(value + 0.1, index, f"  {value}", color="white", ha="left", va="center")
+
+    return chart
