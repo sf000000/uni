8af90bd4439446daa0b1c47d37b86cb4b24411e4
Jawad <bawskiya@gmail.com>
2024-01-25 20:38:58 -0600

    chore(constants.py): add commands_with_tips dictionary back

--- helpers/constants.py
+++ helpers/constants.py
@@ -50,3 +50,11 @@ file_emoji_dict = {
     ".yaml": "🔧",
     ".env": "🔒",
 }
+
+commands_with_tips = {
+    "/emoji add": {
+        "description": "This command allows you to **add new emojis to your server** for quick access. To use it, navigate to [Emoji.gg](<https://emoji.gg>) and browse through the collection of emojis. When you find an emoji you like, click on it to view its details. Look for the **'Emoji ID'** section which typically looks like a number or a short code, such as `5498_catJAM`. **Copy this ID** and use it with the `/emoji add` command by pasting it after the command like so: `/emoji add emoji_id:5498_catJAM`. This will add the emoji to your server.",
+        "example": "```/emoji add emoji_id: 5498_catJAM ```",
+        "image": "https://i.postimg.cc/MKrY4yqr/image.png",
+    }
+}
