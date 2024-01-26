bce25511a01177141b406b1412ffaac18b242175
Jawad <bawskiya@gmail.com>
2024-01-25 20:38:26 -0600

    feat(developer.py): add dev logs command

--- extensions/developer.py
+++ extensions/developer.py
@@ -16,6 +16,7 @@ from helpers.utils import (
     create_bar_chart,
     set_seaborn_style,
 )
+from helpers import emojis
 
 
 def load_config():
@@ -55,6 +56,43 @@ def replace_commit_type_with_emoji(commit_msg):
 
 
 config = load_config()
+
+
+class LogsPaginator(discord.ui.View):
+    def __init__(self, pages: list[str]):
+        super().__init__()
+        self.pages = pages
+        self.current_page = 0
+
+    @discord.ui.button(emoji=emojis._prev, style=discord.ButtonStyle.gray)
+    async def previous(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        if self.current_page == 0:
+            return
+
+        self.current_page -= 1
+        await self.update_page(interaction)
+
+    @discord.ui.button(emoji=emojis._next, style=discord.ButtonStyle.gray)
+    async def next(self, button: discord.ui.Button, interaction: discord.Interaction):
+        if self.current_page == len(self.pages) - 1:
+            return
+
+        self.current_page += 1
+        await self.update_page(interaction)
+
+    @discord.ui.button(label="Clear Logs", style=discord.ButtonStyle.red)
+    async def clear(self, button: discord.ui.Button, interaction: discord.Interaction):
+        with open("bot.log", "w", encoding="utf-8") as f:
+            f.write("")
+
+        await interaction.response.edit_message(content="Cleared logs.", view=None)
+
+    async def update_page(self, interaction: discord.Interaction):
+        await interaction.response.edit_message(
+            content=f"```py\n{self.pages[self.current_page]}```"
+        )
 
 
 class Developer(commands.Cog):
@@ -525,6 +563,27 @@ class Developer(commands.Cog):
 
         await ctx.respond(file=discord.File(filename))
         os.remove(filename)
+
+    @_dev.command(
+        name="logs",
+        description="Shows the bot's logs.",
+    )
+    async def _logs(self, ctx: discord.ApplicationContext):
+        with open("bot.log", "r", encoding="utf-8") as f:
+            logs = f.read()
+
+        if not logs:
+            return await ctx.respond("Logs are empty.", ephemeral=True)
+
+        logs = logs.split("\n")
+        logs.reverse()
+
+        pages = []
+        for i in range(0, len(logs), 10):
+            pages.append("\n".join(logs[i : i + 10]))
+
+        view = LogsPaginator(pages)
+        await ctx.respond(content=f"```py\n{pages[0]}```", view=view)
 
     @commands.Cog.listener()
     async def on_application_command_completion(self, ctx: discord.ApplicationContext):
