c23603a78aedc672d93efbce2493f91b48bd9478
notjawad <bawskiya@gmail.com>
2024-04-02 20:38:56 -0500

    fix(spotify.py): rename Server class to Spotify class

--- extensions/spotify.py
+++ extensions/spotify.py
@@ -4,10 +4,10 @@ from discord.ext import commands
 # TODO: Implement Spotify commands
 
 
-class Server(commands.Cog):
+class Spotify(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
 
 
 def setup(bot: discord.Bot):
-    bot.add_cog(Server(bot))
+    bot.add_cog(Spotify(bot))
